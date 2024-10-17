eb95934906cfc343529107b57bf52482de45e400
sportsmonisha <163819456+sportsmonisha@users.noreply.github.com>
2024-10-17 13:12:46 +0530

    Load to login

--- frontend/src/components/user/login.js
+++ frontend/src/components/user/login.js
@@ -70,7 +70,7 @@ export default function Login() {
                             className="btn btn-block py-3"
                             disabled={loading}
                         >
-                            {loading ? 'Loading...' : 'LOGIN'}
+                        LOGIN
                         </button>
 
                         
